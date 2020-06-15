d6234639e85e96351f5bf70d05d5d9c2a7989f9f
Andrew Smith <a.smith@f5.com>
2020-06-15 14:39:41 -0700

    Added basic infrastructure and example utility

---
+++ test/unit/utils/arrayUtils.js
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2020. F5 Networks, Inc. See End User License Agreement ("EULA") for
+ * license terms. Notwithstanding anything to the contrary in the EULA, Licensee
+ * may copy and modify this software product for its internal business purposes.
+ * Further, Licensee may upload, publish and distribute the modified version of
+ * the software product on devcentral.f5.com.
+ */
+
+'use strict';
+
+const assert = require('assert');
+
+const arrayUtils = require('../../../src/utils/arrayUtils.js');
+
+describe('arrayUtils', () => {
+    describe('.ensureArray', () => {
+        it('should return an empty array', () => {
+            assert.deepStrictEqual(arrayUtils.ensureArray(), []);
+        });
+
+        it('should return an empty array', () => {
+            assert.deepStrictEqual(arrayUtils.ensureArray({}), [{}]);
+        });
+
+        it('should return an array with values in it', () => {
+            assert.deepStrictEqual(arrayUtils.ensureArray({ foo: 'bar' }), [{ foo: 'bar' }]);
+        });
+
+        it('should return the same object as before', () => {
+            assert.deepStrictEqual(arrayUtils.ensureArray(['foo-bar']), ['foo-bar']);
+        });
+    });
+});
